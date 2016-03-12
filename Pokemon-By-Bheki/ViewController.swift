6c846d06a8e31436ea9892c83d73d14fcc6e3538
Bheki Thomola <bhekithomola@bhekis-mbp.home>
2016-03-12 13:29:09 +0000

    Initial Commit

---
+++ Pokemon-By-Bheki/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pokemon-By-Bheki
+//
+//  Created by Bheki Thomola on 12/03/2016.
+//  Copyright © 2016 Wotogs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
